1edcbf43fb805f4c6c9dbfaa7600c69909cc12c7
Ethan Townsend <eetfitness@gmail.com>
2019-06-22 14:41:08 -0700

    adding week 8 assignments

---
+++ week-8/townsend_calculator.py
@@ -0,0 +1,18 @@
+# addition funtcion
+def addition(val1, val2):
+    return val1 + val2
+
+print(addition(1, 2))
+
+# subtraction function
+
+def subtraction(val1, val2):
+    return val1 - val2
+
+print(subtraction(6, 3))
+
+# division function
+def division(val1, val2):
+    return val1/val2
+
+print(division(9, 3))
